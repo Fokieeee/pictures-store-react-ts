cb839880d430bbce942c55bd08838ea3a51dcf7f
Fokieeee <114172954+Fokieeee@users.noreply.github.com>
2023-01-25 11:31:47 +0500

    prepared for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pictures-store-react-ts/",
   plugins: [react()],
-	server: {
+  server: {
     port: 8080,
-  }
-})
+  },
+});
